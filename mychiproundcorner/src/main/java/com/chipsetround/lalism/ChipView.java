3c6dacc57a58ee6750586687563915cad726149b
Sreelal <sreelal8026@gmail.com>
2018-01-03 11:23:49 +0530

    log clear

--- mychiproundcorner/src/main/java/com/chipsetround/lalism/ChipView.java
+++ mychiproundcorner/src/main/java/com/chipsetround/lalism/ChipView.java
@@ -9,10 +9,10 @@ import android.net.Uri;
 import android.support.annotation.ColorInt;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.constraint.ConstraintLayout;
 import android.util.AttributeSet;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.chipsetround.lalism.data.Chip;
@@ -46,7 +46,7 @@ public class ChipView extends FrameLayout {
     private ColorStateList backgroundColor;
     private boolean edittextvisible;
 
-    private ConstraintLayout layout;
+    private LinearLayout layout;
 
 
     public ChipView(@NonNull Context context) {
